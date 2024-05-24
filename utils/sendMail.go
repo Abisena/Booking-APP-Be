76f0f176368af89a914e192d5e5d9a3c750d4465
absnnPut <putsenbi@gmail.com>
2024-05-24 10:04:20 +0700

    Pembaruan Go

--- utils/sendMail.go
+++ utils/sendMail.go
@@ -6,17 +6,17 @@ import (
 )
 
 
-func SendMail(subject string, html string, to []string){
+func SendMail(subject string, body string, to []string){
 	auth := smtp.PlainAuth(
 		"",
 		"abisena1123@gmail.com",
-		"231222",
+		"swojswzpimauiihl",
 		"smtp.gmail.com",
 	)
 
-	headers := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";"
+	// headers := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";"
 
-	msg := "Subject" + subject + "\n" + headers + "\n\n" + html
+	msg := "Subject: " + subject + "\n" + body
 
 	err := smtp.SendMail(
 		"smtp.gmail.com:587",
