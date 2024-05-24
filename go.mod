6dccaa8ee22701664a4c5e783bd42aa303027911
absnnPut <putsenbi@gmail.com>
2024-05-24 15:02:24 +0700

    Pembaruan Go

--- go.mod
+++ go.mod
@@ -2,9 +2,4 @@ module booking-app
 
 go 1.22.0
 
-require (
-	github.com/aymerick/douceur v0.2.0 // indirect
-	github.com/gorilla/css v1.0.0 // indirect
-	github.com/microcosm-cc/bluemonday v1.0.26
-	golang.org/x/net v0.17.0 // indirect
-)
+require github.com/joho/godotenv v1.5.1 // indirect
--- utils/sendMail.go
+++ utils/sendMail.go
@@ -3,14 +3,28 @@ package utils
 import (
 	"fmt"
 	"net/smtp"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file")
+	}
+}
+
 func SendMail(subject string, body string, to []string){
+	email := os.Getenv("EMAIL_APP")
+	password := os.Getenv("SANDI_APP")
+
+
 	auth := smtp.PlainAuth(
 		"",
-		"abisena1123@gmail.com",
-		"swojswzpimauiihl",
+		email,
+		password,
 		"smtp.gmail.com",
 	)
 
