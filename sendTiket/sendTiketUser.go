fb1aecf9fbd3bac86346f2c8c064f71668fbe265
absnnPut <putsenbi@gmail.com>
2024-05-24 10:06:10 +0700

    Pembaruan Go

--- sendTiket/sendTiketUser.go
+++ sendTiket/sendTiketUser.go
@@ -11,7 +11,7 @@ import (
 var wg = sync.WaitGroup{}
 
 func SendTiketUser(firstName string, lastName string, email string, beliTiket uint) {
-	time.Sleep(20 *time.Second)
+	time.Sleep(5 *time.Second)
 	var tiket = fmt.Sprintf("%v tikets akan diberikan ke %v %v", beliTiket, firstName, lastName)
 	fmt.Println("#################")
 	fmt.Printf("Sending ticket:\n %v \nto email address %v\n", tiket, email)
